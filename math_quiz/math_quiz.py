3fb12201120e5eee11170f96a7c15fec04ab1ac1
adithyan_sukumar <adithyansukumar008@gmail.com>
2024-11-04 15:15:22 +0100

    made all necessary changes to code

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,38 +1,60 @@
 import random
 
-
-def function_A(min, max):
+def generate_random_integer(min: int, max: int) -> int:
     """
-    Random integer.
+    Generates a random integer.
     """
-    return random.randint(min, max)
+
+    if isinstance(min,int) == False or isinstance(max,int) == False:
+        print("Only pass integer values as threshold")
+        return 0
+    
+    return random.randint(min, max) # selecting random value between given thresholds
 
 
-def function_B():
-    return random.choice(['+', '-', '*'])
+def generate_random_choice() -> str:
+    """
+    Generates a random choice.
+    """
+    return random.choice(['+', '-', '*']) # select a random choice from given actions
 
 
-def function_C(n1, n2, o):
+def solve(n1: int, n2: int, o: str) -> (str,int):
+    """
+    Solves the given problem and returns the answer.
+    """
     p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+    # applying the chosen operation to numbers
+    try:
+        if o == '+': a = n1 - n2
+        elif o == '-': a = n1 + n2
+        else: a = n1 * n2
+        return p, a
+    except:
+        print("Enter correct values")
 
 def math_quiz():
+    """
+    Starts the math quiz game.
+    """
     s = 0
-    t_q = 3.14159265359
+    t_q = 3
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        # generating random values from n1,n2 & o
+        n1 = generate_random_integer(1, 10); n2 = generate_random_integer(1, 5); o = generate_random_choice()
+
+        PROBLEM, ANSWER = solve(n1, n2, o) # solving the equation and comparing with the user's answer
         print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        try:
+            useranswer = input("Your answer: ")
+            useranswer = int(useranswer)
+        except ValueError:
+            print("Enter valid inputs")
 
         if useranswer == ANSWER:
             print("Correct! You earned a point.")

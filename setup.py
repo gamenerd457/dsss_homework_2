d543b6f80e6966adffd124e55530e6ffeca06106
adithyan_sukumar <72939103+gamenerd457@users.noreply.github.com>
2024-11-04 21:49:42 +0530

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dsss_homework_2",
+    version="0.1",
+    packages=find_packages(),
+    description="math equation solver",
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="https://github.com/gamenerd457/dsss_homework_2",
+)
